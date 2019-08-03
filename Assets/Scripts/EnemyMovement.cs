79e2248582b41823c9c2a5067f79f856d2507d79
davjs <davv.js@gmail.com>
2019-08-03 17:22:39 +0200

    Enemy cone WIP

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour {
     public float DiscoverPlayerRadius = 10.0f;
+    public GameObject Face;
     public float Speed = 10.0f;
     private GameObject _player;
     private Rigidbody2D _body;
 
     private Vector2 _currentDirection;
+    private Vector2 _nextDirection;
 
     private void Awake() {
         _player = GameObject.FindWithTag("Player");
@@ -16,6 +18,7 @@ public class EnemyMovement : MonoBehaviour {
         _body.gravityScale = 0;
         _body.drag = Speed / 5.0f;
         _currentDirection = Random.insideUnitCircle;
+        _nextDirection = Random.insideUnitCircle;
     }
 
     private void Update() {
@@ -26,13 +29,17 @@ public class EnemyMovement : MonoBehaviour {
         else {
             _body.AddForce(_currentDirection * Speed, ForceMode2D.Force);
 
-            if (Random.value < 0.08f) {
-                _currentDirection = Random.insideUnitCircle;
+            if (Random.value < 0.04f) {
+                _nextDirection = Random.insideUnitCircle;
             }
         }
+
+        _currentDirection = Vector2.MoveTowards(_currentDirection, _nextDirection, 0.01f);
+        var angle = Vector2.SignedAngle(Vector2.right, _currentDirection);
+        Face.transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 
-    private void OnCollisionEnter(Collision other) {
+    private void OnCollisionEnter2D(Collision2D other) {
         if (Random.value < 0.2f) {
             _currentDirection = Random.insideUnitCircle;
         }
