f6f82d90ce0ccb07b1f5f2ecf1d36e1b643880e6
davjs <davv.js@gmail.com>
2019-08-03 12:40:14 +0200

    Renames

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -3,30 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour {
-    public float discoverPlayerRadius = 10.0f;
-    public float speed = 10.0f;
+    public float DiscoverPlayerRadius = 10.0f;
+    public float Speed = 10.0f;
     private GameObject _player;
-    private Rigidbody2D body;
+    private Rigidbody2D _body;
 
-    private Vector2 currentDirection = Random.insideUnitCircle;
+    private Vector2 _currentDirection;
 
     private void Awake() {
         _player = GameObject.FindWithTag("Player");
-        body = GetComponent<Rigidbody2D>();
-        body.gravityScale = 0;
-        body.drag = speed / 5.0f;
+        _body = GetComponent<Rigidbody2D>();
+        _body.gravityScale = 0;
+        _body.drag = Speed / 5.0f;
+        _currentDirection = Random.insideUnitCircle;
     }
 
-    void Update() {
-        if (Vector2.Distance(_player.transform.position, transform.position) <= discoverPlayerRadius) {
+    private void Update() {
+        if (Vector2.Distance(_player.transform.position, transform.position) <= DiscoverPlayerRadius) {
             var direciton = (_player.transform.position - transform.position).normalized;
-            body.AddForce(direciton * speed * 1.1f, ForceMode2D.Force );
+            _body.AddForce(direciton * Speed * 1.1f, ForceMode2D.Force);
         }
         else {
-            body.AddForce(currentDirection * speed, ForceMode2D.Force);
+            _body.AddForce(_currentDirection * Speed, ForceMode2D.Force);
 
             if (Random.value < 0.08f) {
-                currentDirection = Random.insideUnitCircle;
+                _currentDirection = Random.insideUnitCircle;
             }
         }
     }
