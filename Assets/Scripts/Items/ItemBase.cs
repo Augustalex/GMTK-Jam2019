30b956ce61597d57b2159ff214686a827ba7d67a
IsacD <isac_degerborg@hotmail.com>
2019-08-03 15:14:01 +0200

    Added item base scripts (WIP)

---
+++ Assets/Scripts/Items/ItemBase.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class ItemBase : MonoBehaviour
+{
+	private void Reset()
+	{
+		GetComponent<CircleCollider2D>().isTrigger = true;
+	}
+
+	void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+	public void Pickup()
+	{
+
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(collision.gameObject.tag == "Player")
+		{
+			var itemHolder = collision.GetComponent<PlayerItemHolder>();
+
+			itemHolder.nearbyItem = this;
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.gameObject.tag == "Player")
+		{
+			var itemHolder = collision.GetComponent<PlayerItemHolder>();
+			if(itemHolder.nearbyItem == this)
+			{
+				itemHolder.nearbyItem = null;
+			}
+
+		}
+	}
+}
---
+++ Assets/Scripts/Items/PlayerItemHolder.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerItemHolder : MonoBehaviour
+{
+	public ItemBase heldItem;
+	public ItemBase nearbyItem;
+
+	public Transform itemHeldPosition;
+
+    void Awake()
+    {
+		if (itemHeldPosition == null)
+			itemHeldPosition = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			PickupItem();
+		}
+    }
+
+	private void PickupItem()
+	{
+		if (nearbyItem == null)
+			return;
+
+		heldItem = nearbyItem;
+		nearbyItem = null;
+
+		heldItem.transform.position = itemHeldPosition.position;
+		heldItem.transform.parent = itemHeldPosition;
+	}
+
+	private void DropItem()
+	{
+		if (heldItem == null)
+			return;
+
+
+
+	}
+}
