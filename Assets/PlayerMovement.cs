660c50ec7d8a01ba9c95314cf526284446e4039e
augustalex <augustalexandersson@gmail.com>
2019-08-03 13:08:51 +0200

    Simple player

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        if (horizontal > 0 || horizontal < 0)
+        {
+            var horizontalVector = new Vector3(horizontal, 0, 0);
+
+            transform.position = transform.position + horizontalVector * Time.deltaTime * 30;
+            GetComponent<Animator>().SetTrigger("Walk");
+
+            if (horizontal < 0)
+            {
+                var scale = transform.localScale;
+                if (scale.x > 0)
+                {
+                    transform.localScale = new Vector3(-1, scale.y, scale.z);
+                }
+            }
+            else if (horizontal > 0)
+            {
+                var scale = transform.localScale;
+                if (scale.x < 0)
+                {
+                    transform.localScale = new Vector3(1, scale.y, scale.z);
+                }
+            }
+        }
+        else
+        {
+            GetComponent<Animator>().SetTrigger("Idle");
+        }
+    }
+}
