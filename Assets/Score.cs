82c45e7b307f0c2f9584f9b7e3a3739b79db88e3
augustalex <augustalexandersson@gmail.com>
2019-08-03 17:38:01 +0200

    Added a simple score text

---
+++ Assets/Score.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    private EndGameDoor _endGameDoor;
+    private Text _text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _text = GetComponent<Text>();
+        _endGameDoor = GameObject.FindGameObjectWithTag("EndGameDoor").GetComponent<EndGameDoor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_endGameDoor.HasEnded())
+        {
+            _text.text = "You survived with " + _endGameDoor.GetScore() + " stuff";
+        }
+    }
+}
---
+++ Assets/Scripts/EndGameDoor.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+public class EndGameDoor : MonoBehaviour
+{
+    private Cart _cart;
+    private bool _hasEnded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cart = GameObject.FindGameObjectWithTag("Cart").GetComponent<Cart>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        _hasEnded = true;
+    }
+
+    public int GetScore()
+    {
+        return _cart.Score;
+    }
+
+    public bool HasEnded()
+    {
+        return _hasEnded;
+    }
+}
