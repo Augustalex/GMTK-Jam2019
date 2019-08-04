bc48cec375174978935c14f3ab0bcc2103c95d4b
augustalex <augustalexandersson@gmail.com>
2019-08-04 14:09:00 +0200

    Sounds

--- Assets/Scripts/Cart.cs
+++ Assets/Scripts/Cart.cs
@@ -1,11 +1,29 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Cart : MonoBehaviour
 {
     public int Score;
+    public AudioClip CartPush;
+    private AudioSource _audioSource;
+
+    public void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.clip = CartPush;
+    }
 
     public void AddItem()
     {
         Score += 1;
+    }
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (!_audioSource.isPlaying || Random.value < .3f)
+        {
+            _audioSource.Play();
+        }
     }
 }
